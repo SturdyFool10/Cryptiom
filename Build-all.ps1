7e8636ab1ad2cb88f8469db3f8174160c535b9fb
Sturdyfool10 <sturdyfool10@pm.me>
2025-01-03 18:36:21 -0700

    Init for first push

---
+++ Build-all.ps1
@@ -0,0 +1,15 @@
+Write-Host "Building Client Debug..."
+./Client_Build_Debug.ps1
+Write-Host "Client Debug build complete.`n"
+
+Write-Host "Building Client Release..."
+./Client_Build_Release.ps1
+Write-Host "Client Release build complete.`n"
+
+Write-Host "Building Server Debug..."
+./Server_Build_Debug.ps1
+Write-Host "Server Debug build complete.`n"
+
+Write-Host "Building Server Release..."
+./Server_Build_Release.ps1
+Write-Host "Server Release build complete.`n"
---
+++ Client/premake5.lua
@@ -0,0 +1,46 @@
+-- premake5.lua
+require "export-compile-commands"
+
+workspace "Client"
+configurations { "Debug", "Release" }
+language "C++"
+cppdialect "C++17"
+architecture "x86_64"
+toolset "clang"
+targetdir "bin/%{cfg.buildcfg}"
+objdir "bin-int/%{cfg.buildcfg}"
+
+filter "action:not vs*"
+symbols "On"
+
+project "Client"
+kind "ConsoleApp"
+location "build"
+
+files {
+    "src/**.c",
+    "src/**.h",
+    "src/**.cpp",
+    "src/**.hpp"
+}
+
+includedirs { "deps/include" }
+libdirs {
+    "deps/libs(Static)",
+    "deps/libs(dynamic)"
+}
+
+filter "configurations:Debug"
+defines { "DEBUG" }
+runtime "Debug"
+optimize "Off"
+
+filter "configurations:Release"
+defines { "RELEASE" }
+runtime "Release"
+optimize "Full"
+
+-- Export compile commands for both Debug and Release configurations
+if _ACTION == "export-compile-commands" then
+    require "export-compile-commands"
+end
